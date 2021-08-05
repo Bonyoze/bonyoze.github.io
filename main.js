b877787f44e745e4f4d025a973b05da9c8d98394
Bonyoze <ravaloni.emv@gmail.com>
2021-08-05 16:55:55 -0400

    update

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
+var cameraFOV = 1.5,
+mouseSensitivity = 1000,
+movementForce = 1.5,
+jumpForce = 0.75;
+
 const init = async () => {
     const canvas = document.getElementById("canvas"),
     engine = new BABYLON.Engine(canvas, true);
 
     // setup scene
     var scene = new BABYLON.Scene(engine);
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 50, -100), scene);
-    camera.fov = 1.5;
+
+    const isGameActive = () => {
+        return document.pointerLockElement === canvas;
+    }
+
+    // setup camera
+    var camera = new BABYLON.FreeCamera("playerCamera", new BABYLON.Vector3(0, 50, -100), scene);
+    camera.fov = cameraFOV;
     camera.minZ = 0.1;
-    camera.maxCameraSpeed = 1;
-    camera.attachControl(canvas, true);
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
+
+    // setup lights and shadows
+    var skyLight = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(-1, -1, 1), scene);
+    skyLight.intensity = 0.4;
+
+    var dirLight = new BABYLON.DirectionalLight("light2", new BABYLON.Vector3(-1, -1, 1), scene);
+    dirLight.intensity = 0.8;
+    dirLight.position = new BABYLON.Vector3(0, 100, 0);
+
+    var shadowGenerator = new BABYLON.ShadowGenerator(2048, dirLight);
+    shadowGenerator.setDarkness(0.5);
+    shadowGenerator.useBlurExponentialShadowMap = true;
+    shadowGenerator.blurKernel = 16;
+    shadowGenerator.useKernelBlur = true;
     
-    // enable physics
+    // setup physics
     scene.enablePhysics(new BABYLON.Vector3(0, -20, 0), new BABYLON.CannonJSPlugin());
 
     // create skybox
@@ -25,16 +46,31 @@ const init = async () => {
     skybox.material.specularColor = new BABYLON.Color3();
 
     // create ground collider
-    var ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 200, height: 200 }, scene);
-    ground.material = new BABYLON.GridMaterial("groundMaterial", scene);
-    ground.material.mainColor = new BABYLON.Color3(1, 1, 1);
-    ground.material.lineColor = new BABYLON.Color3();
-    ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.01, restitution: 0 }, scene);
+    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "assets/heightMap.png", 1000, 1000, 100, 0, 20, scene, false, () => {
+        ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.HeightmapImpostor, { mass: 0, friction: 0.1, restitution: 0 }, scene);
+        ground.material = new BABYLON.StandardMaterial("ground", scene);
+        ground.material.diffuseTexture = new BABYLON.Texture("assets/grass.jpg", scene);
+        ground.material.diffuseTexture.uScale = ground.material.diffuseTexture.vScale = 200;
+        ground.material.specularColor = ground.material.emissiveColor = new BABYLON.Color3(0, 0, 0);
+    });
+    ground.receiveShadows = true;
+    ground.position.y -= 20;
+
+    var platform = BABYLON.MeshBuilder.CreateBox("platform", { height: 5, width: 50, depth: 50});
+    platform.physicsImpostor = new BABYLON.PhysicsImpostor(platform, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.01, restitution: 0 }, scene);
+    platform.material = new BABYLON.StandardMaterial("platform", scene);
+    platform.material.diffuseTexture = new BABYLON.Texture("assets/concrete.jpg", scene);
+    platform.material.diffuseTexture.uScale = platform.material.diffuseTexture.vScale = 5;
+    platform.material.specularColor = platform.material.emissiveColor = new BABYLON.Color3(0, 0, 0);
+    shadowGenerator.addShadowCaster(platform);
+    platform.receiveShadows = true;
+    platform.position.y -= 9.5;
 
     // create player
-    var player = BABYLON.MeshBuilder.CreateCylinder("player", { height: 2, diameter: 1, tesselation: 48 });
+    var player = BABYLON.MeshBuilder.CreateCylinder("player", { height: 2, diameter: 1.15, tesselation: 48 });
+    player.physicsImpostor = new BABYLON.PhysicsImpostor(player, BABYLON.PhysicsImpostor.CylinderImpostor, { mass: 15, friction: 0, restitution: 0 }, scene);
+    player.isPickable = false;
     player.position = new BABYLON.Vector3(0, 1, 0);
-    player.physicsImpostor = new BABYLON.PhysicsImpostor(player, BABYLON.PhysicsImpostor.CylinderImpostor, { mass: 12, friction: 0, restitution: 0 }, scene);
 
     skybox.parent = player;
     skybox.position = player.position
@@ -42,17 +78,22 @@ const init = async () => {
     camera.position = new BABYLON.Vector3(0, 0.75, 0);
 
     // test physics objects
-    for (let i = 0; i < 10; i++) {
-        var cube = BABYLON.MeshBuilder.CreateBox("cube", { size: 10 - i });
-        cube.material = new BABYLON.GridMaterial("cubeMaterial", scene);
-        cube.material.mainColor = new BABYLON.Color3.Random();
-        cube.material.lineColor = new BABYLON.Color3();
-        cube.position.z += 30;
-        cube.position.y += (5 + i * 10);
-        cube.physicsImpostor = new BABYLON.PhysicsImpostor(cube, BABYLON.PhysicsImpostor.BoxImpostor, { mass: (10 - i) * 4, friction: 0.85, restitution: 0 }, scene);
+    for (let i = 0; i < 20; i++) {
+        var cube = BABYLON.MeshBuilder.CreateBox("cube", { size: 20 - i });
+        cube.physicsImpostor = new BABYLON.PhysicsImpostor(cube, BABYLON.PhysicsImpostor.BoxImpostor, { mass: (40 - i) * 4, friction: 0.85, restitution: 0 }, scene);
+        cube.material = new BABYLON.StandardMaterial("ground", scene);
+        cube.material.diffuseTexture = new BABYLON.Texture("assets/checker.jpg", scene);
+        cube.material.diffuseTexture.uScale = cube.material.diffuseTexture.vScale = 2;
+        let hue = Math.random();
+        new BABYLON.Color3.HSVtoRGBToRef(hue * 360, 1, 1, cube.material.diffuseColor);
+        new BABYLON.Color3.HSVtoRGBToRef(hue * 360, 1, 0.5, cube.material.emissiveColor);
+        shadowGenerator.addShadowCaster(cube);
+        cube.receiveShadows = true;
+        cube.position.z += 200;
+        cube.position.y += (10 + i * 20);
     }
 
-    // some vars
+    // handle key inputs
     var keysActive = false,
     forward = false,
     left = false,
@@ -60,7 +101,6 @@ const init = async () => {
     right = false,
     jump = false;
 
-    // handle key inputs
     const keyCallback = (mode, key) => {
         switch (key) {
             case "w":
@@ -80,6 +120,7 @@ const init = async () => {
                 break;
             case "r":
                 if (!mode) {
+                    player.physicsImpostor.setLinearVelocity(new BABYLON.Vector3());
                     player.position = new BABYLON.Vector3(0, 1, 0);
                     camera.rotation = new BABYLON.Vector3();
                 }
@@ -93,12 +134,26 @@ const init = async () => {
 
     // handle pointer lock
     const lockCallback = () => {
-        scene.physicsEnabled = document.pointerLockElement === canvas;
+        scene.physicsEnabled = isGameActive(); // basically pauses the game
     }
 
     document.addEventListener("pointerlockchange", lockCallback, false);
     document.addEventListener("mozpointerlockchange", lockCallback, false);
     document.addEventListener("webkitpointerlockchange", lockCallback, false);
+
+    // handle mouse movement
+    canvas.onmousemove = e => {
+        if (!isGameActive()) return;
+
+        let {
+            movementX,
+            movementY
+        } = e;
+
+        let rot = camera.rotation.add(new BABYLON.Vector3(movementY / mouseSensitivity, movementX / mouseSensitivity));
+        rot.x = Math.min(Math.max(rot.x, -85 * (Math.PI / 180)), 85 * (Math.PI / 180)); // clamp camera pitch
+        camera.rotation = rot;
+    };
 
     canvas.onclick = () => {
         canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock || canvas.webkitRequestPointerLock;
@@ -111,9 +166,17 @@ const init = async () => {
     };
 
     scene.registerBeforeRender(() => {
+        if (!scene.physicsEnabled) return;
+
+        /*let onGround,
+        ray = new BABYLON.Ray(player.position.add(new BABYLON.Vector3(0, 0, 0)), new BABYLON.Vector3(0, -1, 0), 2),
+        pick = scene.pickWithRay(ray);
+        if (pick) onGround = pick.hit;
+        console.log(onGround);*/
+
         player.rotationQuaternion = new BABYLON.Quaternion.RotationAxis(new BABYLON.Vector3(), Math.PI / 3); // prevent rotation
 
-        player.physicsImpostor.setLinearVelocity(new BABYLON.Vector3.Lerp(player.physicsImpostor.getLinearVelocity(), new BABYLON.Vector3(0, player.physicsImpostor.getLinearVelocity().y, 0), 0.1));
+        /*if (onGround) */player.physicsImpostor.setLinearVelocity(new BABYLON.Vector3.Lerp(player.physicsImpostor.getLinearVelocity(), new BABYLON.Vector3(0, player.physicsImpostor.getLinearVelocity().y, 0), 0.1));
 
         let dir = camera.getTarget().subtract(camera.position); // camera direction
 
@@ -127,20 +190,16 @@ const init = async () => {
 
         movement = movement.normalize();
 
-        applyPlayerVel(movement);
+        applyPlayerVel(movement.multiply(new BABYLON.Vector3(movementForce, movementForce, movementForce)));
 
         // apply jump velocity
-        if (jump) applyPlayerVel(new BABYLON.Vector3(0, 0.75, 0));
+        if (jump) applyPlayerVel(new BABYLON.Vector3(0, jumpForce, 0));
     });
 
     // render scene
     engine.runRenderLoop(() => {
         scene.render();
     });
-
-    // initial lock
-    canvas.requestPointerLock();
-    scene.physicsEnabled = document.pointerLockElement === canvas;
 };
 
 window.addEventListener("DOMContentLoaded", init);
